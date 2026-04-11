946b2e2c9ff7943babeac9f43d53581a80d10959
jack <275287908@qq.com>
2026-04-11 21:23:31 +0800

    feat: add TaskManager methods to DagScheduler interface

--- dag-task/dag-si/src/main/java/top/ilovemyhome/dagtask/si/service/DagScheduler.java
+++ dag-task/dag-si/src/main/java/top/ilovemyhome/dagtask/si/service/DagScheduler.java
@@ -1,5 +1,6 @@
 package top.ilovemyhome.dagtask.si.service;
 
+import top.ilovemyhome.dagtask.si.TaskInput;
 import top.ilovemyhome.dagtask.si.TaskOutput;
 import top.ilovemyhome.dagtask.si.TaskRecord;
 import top.ilovemyhome.dagtask.si.enums.TaskStatus;
@@ -7,13 +8,15 @@ import top.ilovemyhome.dagtask.si.enums.TaskStatus;
 import java.util.List;
 
 /**
- * DAG Scheduler - Responsible for DAG runtime scheduling and execution triggering.
+ * DAG Scheduler - Responsible for DAG runtime scheduling, execution triggering,
+ * and manual task operations.
  * Handles:
  * <ul>
  *     <li>Starting a DAG execution</li>
  *     <li>Finding ready tasks (all predecessors completed)</li>
  *     <li>Triggering ready tasks to TaskDispatcher for distribution</li>
  *     <li>Callback after task completion to trigger successors</li>
+ *     <li>Manual task operations: run, force complete, kill, hold, resume</li>
  * </ul>
  */
 public interface DagScheduler {
@@ -53,4 +56,47 @@ public interface DagScheduler {
      * @param output    the task output
      */
     void onTaskCompleted(Long taskId, TaskStatus newStatus, TaskOutput output);
+
+    /**
+     * Execute a task synchronously right now.
+     * Useful for testing or manual triggering.
+     *
+     * @param taskId the task ID to execute
+     * @param input  the input parameters
+     * @return the task output after execution
+     */
+    TaskOutput runNow(Long taskId, TaskInput input);
+
+    /**
+     * Force mark a task as completed successfully.
+     * After marking, triggers all ready successor tasks.
+     *
+     * @param taskId the task ID
+     * @param output the output to set
+     */
+    void forceOk(Long taskId, TaskOutput output);
+
+    /**
+     * Force kill (fail) a task.
+     * After marking, no successor tasks will be triggered.
+     *
+     * @param taskId the task ID
+     */
+    void kill(Long taskId);
+
+    /**
+     * Put a task on hold.
+     * The task will not be triggered even if it is ready.
+     *
+     * @param taskId the task ID
+     */
+    void hold(Long taskId);
+
+    /**
+     * Resume a held task.
+     * If the task is ready after resuming, it will be triggered.
+     *
+     * @param taskId the task ID
+     */
+    void resume(Long taskId);
 }
