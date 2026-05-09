29835cdfeb5c1f39e5c815564abef39729decc6c
jack <275287908@qq.com>
2026-05-09 21:26:43 +0800

    clean

--- dag-task/dag-scheduler/src/main/java/top/ilovemyhome/dagtask/scheduler/auth/TokenPusher.java
+++
@@ -1,7 +0,0 @@
-package top.ilovemyhome.dagtask.scheduler.auth;
-
-import top.ilovemyhome.dagtask.si.auth.TokenPushRequest;
-
-public interface TokenPusher {
-    boolean pushToken(String callbackUrl, String nonce, TokenPushRequest request);
-}
